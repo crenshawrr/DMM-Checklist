edfc48ff4ee30b1bf31504a0b8b425cdfa742a8a
Ryan Crenshaw <39629063+crenshawrr@users.noreply.github.com>
2026-01-30 11:06:15 -0500

    main fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./styles/App.css"; // or your actual css file name/case
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<BrowserRouter basename={import.meta.env.BASE_URL}>
-  <App />
-</BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
